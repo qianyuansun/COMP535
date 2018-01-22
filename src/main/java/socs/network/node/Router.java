97b3b64ee837d2431c1207f75c85dd9f3dec4335
Casey <qianyuan.sun@mail.mcgill.ca>
2018-01-22 17:29:09 -0500

    start attach

--- src/main/java/socs/network/node/Router.java
+++ src/main/java/socs/network/node/Router.java
@@ -3,7 +3,16 @@ package socs.network.node;
 import socs.network.util.Configuration;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 
 public class Router {
@@ -14,6 +23,7 @@ public class Router {
 
   //assuming that all routers are with 4 ports
   Link[] ports = new Link[4];
+  
 
   public Router(Configuration config) {
     rd.simulatedIPAddress = config.getString("socs.network.router.ip");
@@ -39,6 +49,11 @@ public class Router {
    */
   private void processDisconnect(short portNumber) {
 
+	  for(Link link: ports){
+		  if(link != null && link.router2.getProcessPortNumber() == portNumber){				  
+			  link = null;
+		  }
+	  }
   }
 
   /**
@@ -50,14 +65,53 @@ public class Router {
    */
   private void processAttach(String processIP, short processPort,
                              String simulatedIP, short weight) {
-
+	  
+	  String msg = "Connection cannot be established." ;
+	  
+	  for(Link link: ports){
+		  if(link == null){
+			  RouterDescription r2 = new RouterDescription(processIP, processPort, simulatedIP);
+			  link = new Link(rd, r2);
+			  msg = "Connection established." ;
+			  break;
+		  }
+	  }
+	  
+	  System.out.println(msg);
   }
 
   /**
    * broadcast Hello to neighbors
    */
   private void processStart() {
+	  //client
+	  try{
+		  Socket client = new Socket(hostName, port);
+		  System.out.println("Connected to "+ client.getRemoteSocketAddress());
+		  OutputStream outToServer = client.getOutputStream();
+		  DataOutputStream message_out = new DataOutputStream(outToServer);
+		  message_out.writeUTF("Hello From " + client.getLocalSocketAddress());
+		  InputStream inToClient = client.getInputStream();
+		  DataInputStream message_in = new DataInputStream(inToClient);
+		  System.out.println(message_in.readUTF());
+		  client.close();
+	  } catch(IOException e){
+		  e.printStackTrace();
+	  }
 
+	  //server
+	  try {
+		  ServerSocket serverSocket = new ServerSocket(rd.getProcessPortNumber());
+		  Socket socket = serverSocket.accept();
+		  //this.ports[0].router2.getSimulatedIPAddress();
+		  //this.ports[0].router2.getProcessPortNumber();
+		  BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		  inFromClient.readLine();
+		  		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
   }
 
   /**
